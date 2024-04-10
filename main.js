533ae4b5c572d1c4e9d4b618fea6c167bae2827f
marianinha1 <108131541+marianinha1@users.noreply.github.com>
2024-04-10 11:06:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
