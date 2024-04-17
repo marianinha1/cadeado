36d861a81c76d94c737ee527ee73492993578326
marianinha1 <108131541+marianinha1@users.noreply.github.com>
2024-04-17 09:59:32 -0300

    Add files via upload

---
+++ cadeado-main/main.js
@@ -0,0 +1,20 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function diminuiTamanho(){
+ 
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
